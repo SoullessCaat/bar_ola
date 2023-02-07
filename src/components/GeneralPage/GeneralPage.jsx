5fd7391f7e9da05635757b565c0b6caafc02cb1e
polina.t <polina.t@ati.su>
2023-02-07 23:15:41 +0300

    main header + general

--- src/components/GeneralPage/GeneralPage.jsx
+++ src/components/GeneralPage/GeneralPage.jsx
@@ -1,9 +1,22 @@
 import React from "react";
 import "./GeneralPage.css"
+import horseLogo from "../../logo/horse.png";
+import barLogo from "../../logo/bar-logo.png"
 
 const GeneralPage = () => {
     return (
         <div className="general-general">
+            <div className="general-bar-description">
+                <div><img src={barLogo} alt="" width={265} height={165}></img></div>
+                <div className="general-description">С учётом сложившейся международной обстановки, граница обучения кадров представляет собой интересный эксперимент проверки модели развития. </div>
+                <div className="general-buttons">
+                    <div>Забронировать</div>
+                    <div>Посмотреть меню</div>
+                </div>
+            </div>
+            <div>
+                <img src={horseLogo} alt="" width={337} height={546} />
+            </div>
         </div>
     )
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import "./Header.css"
+import PinAltFill from "../../logo/PinAltFill.png"
 
 const Header = () => {
     return (
         <div className="header-header">
-
+            <div className="header-menu">
+                <div>главная</div>
+                <div>о баре</div>
+                <div>Меню</div>
+                <div>События</div>
+                <div>контакты</div>
+            </div>
+            <div className="header-adress">
+                <div>СПБ, ул. Каштановая аллея, дом 2</div>
+                <img src={PinAltFill} alt="" width={36} height={36} ></img>
+            </div>
         </div>
     )
 }
